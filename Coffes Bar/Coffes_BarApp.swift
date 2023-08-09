7bbb91ef92a1f0b2d3ae5152fd2c5dbbfcc6ce5d
Kenji <31631601+kenjimaeda54@users.noreply.github.com>
2023-08-09 20:20:58 -0300

    Initial Commit

---
+++ Coffes Bar/Coffes_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffes_BarApp.swift
+//  Coffes Bar
+//
+//  Created by kenjimaeda on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Coffes_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
